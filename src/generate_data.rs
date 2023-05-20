c9a87553dbb9679610aad20140bcd5ad038a2370
nikitavbv@gmail.com <nikitavbv@gmail.com>
2023-05-20 19:04:51 +0200

    feat(data): compute vectors for commoncrawl

--- src/generate_data.rs
+++ src/generate_data.rs
@@ -1,6 +1,10 @@
-use rust_bert::pipelines::sentence_embeddings::{
-    SentenceEmbeddingsBuilder,
-    SentenceEmbeddingsModelType,
+use {
+    std::{fs::File, io::{BufRead, BufReader, Write}},
+    indicatif::ProgressIterator,
+    rust_bert::pipelines::sentence_embeddings::{
+        SentenceEmbeddingsBuilder,
+        SentenceEmbeddingsModelType,
+    },
 };
 
 pub fn generate_data() {
@@ -10,11 +14,21 @@ pub fn generate_data() {
         SentenceEmbeddingsModelType::AllMiniLmL12V2
     ).create_model().unwrap();
 
-    let sentences = [
-        "this is an example sentence",
-        "each sentence is converted"
-    ];
+    let input_file = File::open("./data/commoncrawl/data.warc.wet").unwrap();
+    let lines: Vec<String> = BufReader::new(&input_file).lines().map(|line| line.unwrap()).collect();
+    println!("processing lines");
 
-    let output = model.encode(&sentences).unwrap();
-    println!("output is: {:?}", output);
+    let mut vectors = File::create("./data/commoncrawl/vectors").unwrap();
+
+    for line in lines.into_iter().progress() {
+        let output = model.encode(&[&line]).unwrap();
+        let output = output.get(0).unwrap();
+        let encoded = bincode::serialize(&output).unwrap();
+
+        if encoded.len() != 1544 {
+            panic!("unexpected encoded len for sentence: {}", line);
+        }
+
+        vectors.write(&encoded).unwrap();
+    }
 }
