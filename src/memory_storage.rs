4e2b1a5d1af9b1f1c5054a1179437e58250f4225
Nikita Volobuev <nikitavbv@gmail.com>
2023-07-20 09:44:28 +0300

    feat(storage): implement allocate

--- src/memory_storage.rs
+++ src/memory_storage.rs
@@ -1,11 +1,14 @@
 use {
+    std::collections::HashMap,
     tracing::info,
     tonic::{transport::Server, Status, Request, Response},
+    tokio::sync::Mutex,
     crate::rpc::{
         memory_storage_service_server::{
             MemoryStorageService,
             MemoryStorageServiceServer,
         },
+        MemoryBlockId,
         AllocateMemoryBlockRequest,
         AllocateMemoryBlockResponse,
         WriteMemoryBlockRequest,
@@ -16,6 +19,15 @@ use {
         FreeMemoryBlockResponse,
     }
 };
+
+#[derive(Eq, Hash, PartialEq, Clone)]
+struct BlockId {
+    id: u32,
+}
+
+struct BlockData {
+    data: Vec<u8>,
+}
 
 pub async fn run_memory_storage_server() {
     let addr = "0.0.0.0:9000".parse().unwrap();
@@ -30,11 +42,30 @@ pub async fn run_memory_storage_server() {
 }
 
 struct MemoryStorageServiceHandler {
+    storage: Mutex<HashMap<BlockId, BlockData>>,
+    id_counter: Mutex<u32>,
 }
 
 impl MemoryStorageServiceHandler {
     pub fn new() -> Self {
         Self {
+            storage: Mutex::new(HashMap::new()),
+            id_counter: Mutex::new(0),
+        }
+    }
+
+    async fn next_id(&self) -> BlockId {
+        let mut id_counter = self.id_counter.lock().await;
+        *id_counter += 1;
+
+        BlockId {
+            id: *id_counter,
+        }
+    }
+
+    fn new_block(&self) -> BlockData {
+        BlockData {
+            data: vec![0; 2 * 1024 * 1024],
         }
     }
 }
@@ -42,7 +73,17 @@ impl MemoryStorageServiceHandler {
 #[tonic::async_trait]
 impl MemoryStorageService for MemoryStorageServiceHandler {
     async fn allocate_memory_block(&self, request: Request<AllocateMemoryBlockRequest>) -> Result<Response<AllocateMemoryBlockResponse>, Status> {
-        unimplemented!()
+        let id = self.next_id().await;
+        let block_data = self.new_block();
+
+        let mut storage = self.storage.lock().await;
+        storage.insert(id.clone(), block_data);
+
+        Ok(Response::new(AllocateMemoryBlockResponse { 
+            id: Some(MemoryBlockId {
+                id: id.id,
+            })
+        }))
     }
 
     async fn write_memory_block(&self, request: Request<WriteMemoryBlockRequest>) -> Result<Response<WriteMemoryBlockResponse>, Status> {
