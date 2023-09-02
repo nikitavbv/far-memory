6cab8afc3cdfa8424c603eb82c9196e8dc031cb0
Nikita Volobuev <nikitavbv@gmail.com>
2023-09-03 00:30:30 +0300

    feat(thesis): add abbreviations list

--- thesis-rs/README.md
+++ thesis-rs/README.md
@@ -1,1 +1,5 @@
 [requirements](https://ela.kpi.ua/bitstream/123456789/49978/1/Mahisterska_dysertatsiia.pdf)
+
+[formatting](https://drive.google.com/file/d/1XzGVVvXRREoc6HGYMpjZFywzsWzRa01l/view)
+
+[examples](https://ela.kpi.ua/handle/123456789/21930)
--- thesis-rs/src/sections/abbreviations.rs
+++ thesis-rs/src/sections/abbreviations.rs
@@ -1,4 +1,7 @@
-use docx_rs::{Docx, Run, Paragraph, AlignmentType};
+use {
+    docx_rs::{Docx, Run, Paragraph, AlignmentType, Table, TableRow, TableCell, LineSpacing},
+    crate::components::PlaceholderComponent,
+};
 
 pub trait AbbreviationsListSection {
     fn add_abbreviations_list_section(self) -> Self;
@@ -7,9 +10,25 @@ pub trait AbbreviationsListSection {
 impl AbbreviationsListSection for Docx {
     fn add_abbreviations_list_section(self) -> Self {
         self.add_paragraph(Paragraph::new()
+            .line_spacing(LineSpacing::new().after(300))
             .style("Heading1")
             .page_break_before(true)
             .align(AlignmentType::Center)
-            .add_run(Run::new().add_text("Перелік скорочень".to_uppercase())))
+            .add_run(Run::new().add_text("Перелік умовних позначень, СИМВОЛІВ, ОДИНИЦЬ, СКОРОЧЕНЬ І ТЕРМІНІВ"))
+        )
+            .add_table(Table::new(vec![
+                TableRow::new(vec![
+                    TableCell::new()
+                        .add_paragraph(Paragraph::new().add_placeholder_component("API", "add some real terms")),
+                    TableCell::new()
+                        .add_paragraph(Paragraph::new().add_placeholder_component("– Application programming interface, прикладний програмний Інтерфейс", "add some real terms")),
+                ]),
+                TableRow::new(vec![
+                    TableCell::new()
+                        .add_paragraph(Paragraph::new().add_placeholder_component("Розподілена система", "add some real terms")),
+                    TableCell::new()
+                        .add_paragraph(Paragraph::new().add_placeholder_component("–Програмне забезпечення, що складається з декількох компонентів, що виконуються на різних фізичних вузлах.", "add some real terms")),
+                ])
+            ]).clear_all_border())
     }
 }
