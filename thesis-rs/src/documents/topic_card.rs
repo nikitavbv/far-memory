ce8333433f2de20a3490d2bfa72a70d5ffc5a65c
Nikita Volobuev <nikitavbv@gmail.com>
2023-09-08 23:27:21 +0300

    feat(thesis): add text to topic card

--- thesis-rs/src/documents/topic_card.rs
+++ thesis-rs/src/documents/topic_card.rs
@@ -1,4 +1,4 @@
-use docx_rs::Docx;
+use docx_rs::{Docx, Paragraph, Run, BreakType, AlignmentType};
 
 pub trait TopicCardDocument {
     fn add_topic_card_document(self) -> Self;
@@ -7,5 +7,17 @@ pub trait TopicCardDocument {
 impl TopicCardDocument for Docx {
     fn add_topic_card_document(self) -> Self {
         self
+            .add_paragraph(Paragraph::new()
+            .add_run(Run::new()
+                .size(28)
+                .bold()
+                .add_text("Національний технічний університет України".to_uppercase())
+                .add_break(BreakType::TextWrapping)
+                .add_text("«Київський Політехнічний Інститут".to_uppercase())
+                .add_break(BreakType::TextWrapping)
+                .add_text("імені ")
+                .add_text("Ігоря Сікорського»".to_uppercase())
+            )
+            .align(AlignmentType::Center))
     }
 }
