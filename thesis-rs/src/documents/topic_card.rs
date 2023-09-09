ab043363f32d62389f4ab4538728529080156e7a
Nikita Volobuev <nikitavbv@gmail.com>
2023-09-09 18:35:10 +0300

    feat(thesis): add sections to topic card

--- thesis-rs/src/documents/topic_card.rs
+++ thesis-rs/src/documents/topic_card.rs
@@ -1,23 +1,45 @@
-use docx_rs::{Docx, Paragraph, Run, BreakType, AlignmentType};
+use {
+    docx_rs::{Docx, Paragraph, Run, BreakType, Table, TableRow, TableCell, WidthType, LineSpacing},
+    crate::content::Content,
+};
 
 pub trait TopicCardDocument {
-    fn add_topic_card_document(self) -> Self;
+    fn add_topic_card_document(self, content: &Content) -> Self;
 }
 
 impl TopicCardDocument for Docx {
-    fn add_topic_card_document(self) -> Self {
+    fn add_topic_card_document(self, content: &Content) -> Self {
         self
+            .add_paragraph(Paragraph::new().add_run(Run::new().add_break(BreakType::TextWrapping)))
+            .add_table(Table::new(vec![
+                TableRow::new(vec![
+                    TableCell::new()
+                        .add_paragraph(Paragraph::new().line_spacing(LineSpacing::new().after(100).before(60)).add_run(Run::new().size(2 * 10).add_text("група")))
+                        .width(1500, WidthType::Dxa),
+                    TableCell::new()
+                        .add_paragraph(Paragraph::new().line_spacing(LineSpacing::new().after(100).before(60)).add_run(Run::new().size(2 * 10).add_text("студент")))
+                        .width(6000, WidthType::Dxa),
+                    TableCell::new()
+                        .add_paragraph(Paragraph::new().line_spacing(LineSpacing::new().after(100).before(60)).add_run(Run::new().size(2 * 10).add_text("керівник")))
+                        .width(4000, WidthType::Dxa),
+                ]),
+            ]))
             .add_paragraph(Paragraph::new()
-            .add_run(Run::new()
-                .size(28)
-                .bold()
-                .add_text("Національний технічний університет України".to_uppercase())
-                .add_break(BreakType::TextWrapping)
-                .add_text("«Київський Політехнічний Інститут".to_uppercase())
-                .add_break(BreakType::TextWrapping)
-                .add_text("імені ")
-                .add_text("Ігоря Сікорського»".to_uppercase())
-            )
-            .align(AlignmentType::Center))
+                .line_spacing(LineSpacing::new().before(300))
+                .add_run(Run::new().add_text("Тема").size(2 * 14).color("#434343")))
+            .add_paragraph(Paragraph::new().line_spacing(LineSpacing::new().before(100)).add_run(Run::new().bold().size(2 * 11).add_text(&content.topic)))
+            .add_paragraph(Paragraph::new()
+                .line_spacing(LineSpacing::new().before(300))
+                .add_run(Run::new().add_text("Проблема, що розглядається").size(2 * 14).color("#434343")))
+            .add_paragraph(Paragraph::new().line_spacing(LineSpacing::new().before(100)).add_run(Run::new().add_text("Один абзац про проблему.")))
+            .add_paragraph(Paragraph::new()
+                .line_spacing(LineSpacing::new().before(300))
+                .add_run(Run::new().add_text("Мета").size(2 * 14).color("#434343")))
+            .add_paragraph(Paragraph::new().line_spacing(LineSpacing::new().before(100)).add_run(Run::new().add_text("Підвищити швидкість роботи, якість або надійність (вказати критерії) / розширити рамки використання / забезпечити певну властивість ПЗ, тощо.")))
+            .add_paragraph(Paragraph::new().add_run(Run::new().add_text("Об'єкт дослідження: програмне забезпечення для")))
+            .add_paragraph(Paragraph::new().add_run(Run::new().add_text("Предмет дослідження: процеси розроблення, модифікації, аналізу, забезпечення якості, впровадження і супроводження програмного забезпечення")))
+            .add_paragraph(Paragraph::new()
+                .line_spacing(LineSpacing::new().before(300))
+                .add_run(Run::new().add_text("Задачі, що вирішуються в роботі").size(2 * 14).color("#434343")))
     }
 }
--- thesis-rs/src/main.rs
+++ thesis-rs/src/main.rs
@@ -10,7 +10,7 @@ use {
         StyleType,
     },
     crate::{
-        documents::thesis::ThesisDocument,
+        documents::{ThesisDocument, TopicCardDocument},
         content::Content,
         context::Context,
         utils::init_logging,
@@ -29,6 +29,8 @@ pub mod utils;
 struct Args {   
     #[arg(short, long)]
     pdf: bool,
+    #[arg(short, long)]
+    open: bool,
 }
 
 fn main() {
@@ -36,14 +38,29 @@ fn main() {
 
     let args = Args::parse();
 
-    let path = "./thesis.docx";
-    let file = std::fs::File::create(path).unwrap();
-
-    info!("generating thesis to {:?}", path);
-
     let mut context = Context::new();
     let content = Content::new();
 
+    let path = std::fs::File::create("./output/іп22мп_волобуєв_КАРТКА.docx").unwrap();
+    Docx::new()
+        .page_margin(
+            PageMargin::new()
+                .left(mm_to_twentieth_of_a_point(10.0))
+                .top(mm_to_twentieth_of_a_point(10.0))
+                .bottom(mm_to_twentieth_of_a_point(9.6))
+                .right(mm_to_twentieth_of_a_point(9.7))   
+        )
+        .default_fonts(RunFonts::new().cs("Arial").hi_ansi("Arial"))
+        .default_size(2 * 11)
+        .default_tab_stop(0)
+        .add_topic_card_document(&content)
+        .build()
+        .pack(&path)
+        .unwrap();
+
+    let path = "./output/thesis.docx";
+    let file = std::fs::File::create(path).unwrap();
+    info!("generating thesis to {:?}", path);
     Docx::new()
         .page_margin(
             PageMargin::new()
@@ -59,15 +76,17 @@ fn main() {
         .add_style(Style::new("Heading2", StyleType::Paragraph).name("Heading 2").bold())
         .add_thesis_document(&mut context, &content)
         .build()
-        .pack(file)
+        .pack(&file)
         .unwrap();
 
     if args.pdf {
         info!("converting to pdf");
-        Command::new("docx2pdf").args(["./thesis.docx", "./thesis.pdf"]).output().unwrap();
+        Command::new("docx2pdf").args(["./output/thesis.docx", "./output/thesis.pdf"]).output().unwrap();
     
-        info!("done, opening resulting file");
-        Command::new("open").args(["./thesis.pdf"]).output().unwrap();
+        if args.open {
+            info!("done, opening resulting file");
+            Command::new("open").args(["./thesis.pdf"]).output().unwrap();
+        }
     }
 }
 
