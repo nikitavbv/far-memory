cf6744671b8b4a432f19a8da73bde2f5c06f3e42
Nikita Volobuev <nikitavbv@gmail.com>
2023-09-24 00:24:05 +0300

    feat(thesis): add more research notes about hydra implementation of far memory

--- thesis-rs/src/content/thesis/main_section/research/hydra.rs
+++ thesis-rs/src/content/thesis/main_section/research/hydra.rs
@@ -3,7 +3,7 @@ use crate::engine::{Block, paragraph};
 /**
  * "Hydra : Resilient and Highly Available Remote Memory"
  * see https://www.usenix.org/system/files/fast22-lee.pdf
- * (currently at page 4).
+ * (currently at page 7).
  * 
  * cons:
  * - it uses RDMA (requires NIC that supports it).
@@ -18,7 +18,20 @@ use crate::engine::{Block, paragraph};
  * - remote in-memory erasure coding.
  * - compression.
  * 
+ * Erasure coding works better for larger chunks for data.
  * CodingSets - coding group placement algorithm for erasure-coded data.
+ * 
+ * components:
+ * - resilience manager - coordinates erasure-coded resilience operations during remote read/write.
+ * - resource monitor - handles the memory management in a remote machine.
+ * 
+ * improving reliability:
+ * - different modes of erasure coding
+ * 
+ * improving latency:
+ * - during remote write, resilience manager applies erasure coding to splits, encodes them using Reed-Solomon codes
+ * and decreases latency by avoiding the batch waiting time.
+ * - resilience manager sends the data splits first, then encodes and sens the parity splits asynchronously.
  */
 pub fn hydra() -> Block {
     Block::Multiple(vec![
