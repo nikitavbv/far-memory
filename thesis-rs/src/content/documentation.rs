395a6c9557a313a5a550413f18b9bf075720586f
Nikita Volobuev <nikitavbv@gmail.com>
2023-09-29 23:21:27 +0300

    feat(documentation): definition for far memory

--- thesis-rs/src/content/documentation.rs
+++ thesis-rs/src/content/documentation.rs
@@ -6,6 +6,6 @@ pub fn documentation() -> Block {
         Block::Note(r#"Please note that most parts of documentation for this project are in Ukrainian because I am working on this in scope of my thesis at Kyiv Polytechnic Institute and I
 need to be able to refer to this documentation when talking to thesis supervisors and other people from the university. I will probably add English translation later."#.to_owned()),
         Block::SubsectionHeader("Віддалена памʼять".to_owned()),
-        Block::Placeholder(Box::new(Block::Paragraph("add text explaining what it is".to_owned())), "add text explaining what it is".to_owned()),
+        Block::Placeholder(Box::new(Block::Paragraph("Віддалена памʼять - тип памʼяті що знаходиться на віддалених вузлах у розподіленій системі.".to_owned())), "add text explaining what it is".to_owned()),
     ])
 }
