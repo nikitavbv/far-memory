48c1c023bd6d4fe3fecf61a0d63374a2a202d3fe
Nikita Volobuev <nikitavbv@gmail.com>
2023-10-09 18:01:20 +0300

    feat(documentation): add benchmark results

--- src/thesis/content/documentation.rs
+++ src/thesis/content/documentation.rs
@@ -168,6 +168,7 @@ ML моделі (у тому числі RNN) та ін."#.into(),
           ],
           rows: vec![
             vec!["baseline".to_owned(), "4.95".to_owned(), "27755".to_owned()],
+            vec!["far memory for vocab without swapout".to_owned(), "4.9".to_owned(), "27757".to_owned()],
           ],
         },
 
