16d46749d3042e58ae61b25c58f5c4ab844c1068
Nikita Volobuev <nikitavbv@gmail.com>
2023-10-26 23:16:05 +0300

    feat(thesis): finish improving description for carbink

--- src/thesis/content/thesis/main_section/research/far_memory_in_warehouse_scale.rs
+++ src/thesis/content/thesis/main_section/research/far_memory_in_warehouse_scale.rs
@@ -3,9 +3,9 @@ use crate::thesis::engine::{Block, paragraph, subsection_header};
 /**
  * "Software-Defined Far Memory in Warehouse-Scale Computers"
  * see: https://storage.googleapis.com/pub-tools-public-publication-data/pdf/9bb06ab825a127bef4e33c488eaa659d6856225a.pdf
- * 
+ *
  * "maximize memory savings while meeting performance SLOs".
- * 
+ *
  * - nothing is stored remotely. This is all about compression and decompression with zswap.
  * - goal is to design a robust and effective control plane for latge-scale deployment of zswap (which is far memory).
  * - performance is treated as a first-class constraint.
@@ -24,32 +24,32 @@ use crate::thesis::engine::{Block, paragraph, subsection_header};
  *     - but this threshold is good for the past, not for the current workload. That's why K-th percentile is computed for the past
  *     when picking threshold for next minute.
  *     - ML-based Autotuner picks parameters for Node Agent which peeks Cold Age Threshold.
- * 
+ *
  * - zswap is triggered only when a host memory node runs out of memory and tries to compress pages until it makes enough room to avoid
  * out-of-memory situations.
  *   - the primary difference from existing mechanism is around when to compress pages, or when to migrate pages from near memory to
  *   far memory.
  *   - unlike zswap in the Linux kernel, this system identifies cold memory pages in the background and proactively compresses them,
  *   so that the extra free memory can be used to schedule more jobs to the machine.
- * 
+ *
  * terms
  *  - cold page - memory page that has not been accessed beyond a threshold of T seconds.
  *  - promotion rate - rate of accesses to cold memory pages.
  *    - equivalent to the number of unique pages in far memory that are accessed in a unit of time.
- * 
+ *
  * challenges:
  * - system has to accurately control its aggressiveness to minimize the impact on application performance (i.e. latency should be low).
- * - be resilient to the variation of cold memory behaviour accross different machines, clusters and jobs (i.e. should adapt to the 
+ * - be resilient to the variation of cold memory behaviour accross different machines, clusters and jobs (i.e. should adapt to the
  * environment).
- * 
+ *
  * integration
  * - zswap-based
- * 
+ *
  * cold page identification
  * - relies only on the working set size, promotion histogram and the cold page histogram.
  * - offline what-if analysis is possible.
  * - stats are exported in 5-minute perioud aggregations over telemetry infrastructure.
- * 
+ *
  * autotuning
  *  - uses machine learning to optimize the control plane based on the fleet-wide behaviour.
  *  - fast far memory model estimating behaviour under different configurations.
@@ -65,7 +65,7 @@ use crate::thesis::engine::{Block, paragraph, subsection_header};
 pub fn far_memory_in_warehouse_scale() -> Block {
     Block::Multiple(vec![
         subsection_header("Software-Defined Far Memory in Warehouse-Scale Computers"),
-        Block::Placeholder(
+        /*Block::Placeholder(
             Box::new(paragraph("Компанія Google розробила та в тестовому режимі інтегрувала систему віддаленої памʼяті в своїх центрах обробки даних. Наскільки відомо на момент проведення дослідження існуючих реалізацій для цього курсового проекту, це єдиний великий оператор центрів обробки даних, який спроектував та застосував систему віддаленої памʼяті на великих масштабах та обладнанні яке виконує програмне забезпечення, що обробляє запити від користувачів (іншими словами, на справжньому навантаженні, а не у тестовому середовищі).".to_owned())),
             "improve this text".to_owned(),
         ),
@@ -84,6 +84,6 @@ pub fn far_memory_in_warehouse_scale() -> Block {
         Block::Placeholder(
             Box::new(paragraph("Недоліком системи є те, що її програмний код не є публічно доступним, а окремі компоненти є специфічними для середовища, що використовується в компанії Google.".to_owned())),
             "improve this text".to_owned(),
-        ),
+        ),*/
     ])
 }
