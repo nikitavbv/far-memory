77b5647ff8c1e84283c8c4f94a06485f696823c9
Nikita Volobuev <nikitavbv@gmail.com>
2023-11-13 00:03:24 +0200

    feat(replacement): rnn is working correctly

--- src/client/replacement/rnn.rs
+++ src/client/replacement/rnn.rs
@@ -32,7 +32,7 @@ impl RNNModel {
     pub fn forward(&self, data: &Tensor) -> Tensor {
         let lstm_output = self.lstm.seq(&data).unwrap();
         let lstm_output = self.lstm.states_to_tensor(&lstm_output).unwrap().reshape(&[lstm_output.len(), self.lstm_output_dim]).unwrap();
-        ops::softmax_last_dim(&self.linear.forward(&lstm_output).unwrap()).unwrap()
+        self.linear.forward(&lstm_output).unwrap()
     }
 }
 
@@ -50,19 +50,21 @@ pub fn rnn_training_test() {
         data.push(2);
         data
     };
+    let mut keys: Vec<_> = data.iter().chain(predictions.iter()).collect::<HashSet<_>>().into_iter().cloned().collect();
+    keys.sort();
 
     let data_len = data.len();
     let classes = data.iter().collect::<HashSet<_>>().len();
-    let data: Vec<f32> = one_hot_encode(data).into_iter().flatten().collect();
+    let data: Vec<f32> = one_hot_encode(&keys, data).into_iter().flatten().collect();
     let data = Tensor::from_vec(data.clone(), &[1, data_len, classes], &dev).unwrap();
 
     let predictions_len = predictions.len();
-    let predictions: Vec<f32> = one_hot_encode(predictions).into_iter().flatten().collect();
+    let predictions: Vec<f32> = one_hot_encode(&keys, predictions).into_iter().flatten().collect();
     let predictions = Tensor::from_vec(predictions.clone(), &[1, predictions_len, classes], &dev).unwrap();
 
     let model = RNNModel::new(vs.clone(), classes);
-    let mut adam = candle_nn::AdamW::new_lr(varmap.all_vars(), 1.0).unwrap();
-    for epoch in 0..100 {
+    let mut adam = candle_nn::AdamW::new_lr(varmap.all_vars(), 0.01).unwrap();
+    for _epoch in 0..1000 {
         let output = model.forward(&data).reshape((1, 27, 15)).unwrap();
         let loss = loss::mse(&output, &predictions).unwrap();
         adam.backward_step(&loss).unwrap();
@@ -70,18 +72,22 @@ pub fn rnn_training_test() {
         println!("loss: {:?}", loss);
     }
 
-    let result = model.forward(&data);
+    let result = model.forward(&Tensor::from_vec(one_hot_encode(&keys, vec![77, 45, 1, 2, 3, 77, 45, 32, 1, 2]).into_iter().flatten().collect(), &[1, 10, classes], &dev).unwrap()).to_vec2::<f32>().unwrap();
+    let result = &result[result.len() - 1];
+    let result = result.iter()
+        .enumerate()
+        .max_by(|(_, a), (_, b)| a.total_cmp(b))
+        .map(|(index, _)| index)
+        .unwrap();
+    let result = keys[result];
     println!("output: {:?}", result);
 }
 
-fn one_hot_encode(data: Vec<u32>) -> Vec<Vec<f32>> {
-    let mut unqiue: Vec<_> = data.iter().collect::<HashSet<_>>().into_iter().collect();
-    unqiue.sort();
-
+fn one_hot_encode(key: &Vec<u32>, data: Vec<u32>) -> Vec<Vec<f32>> {
     let mut result = Vec::new();
     for item in &data {
-        let mut entry = vec![0.0; unqiue.len()];
-        entry[unqiue.iter().position(|v| *v == item).unwrap()] = 1.0;
+        let mut entry = vec![0.0; key.len()];
+        entry[key.iter().position(|v| v == item).unwrap()] = 1.0;
         result.push(entry);
     }
 
