0a5cb441d1e2d53bbb180d3f5e331aae04904104
Nikita Volobuev <nikitavbv@gmail.com>
2023-12-09 18:37:25 +0200

    feat(demo): add far memory client to web service demo

--- src/demo/web_service.rs
+++ src/demo/web_service.rs
@@ -1,9 +1,25 @@
 use {
     std::{collections::HashMap, io::Write, time::Instant, hint::black_box},
-    tracing::info,
+    tracing::{info, warn},
     rand::{RngCore, Rng, rngs::OsRng, prelude::SliceRandom},
     rand_distr::Zipf,
     aes_gcm::{aead::{KeyInit, Aead, AeadCore}, Aes256Gcm},
+    prometheus::Registry,
+    crate::{
+        client::{
+            FarMemoryClient,
+            FarMemoryBackend,
+            NetworkNodeBackend,
+            ErasureCodingBackend,
+            ReplicationBackend,
+            LocalDiskBackend,
+            InstrumentedBackend,
+            PreferRemoteSpansReplacementPolicy,
+            MostRecentlyUsedReplacementPolicy,
+            RemoteReplayReplacementPolicy,
+        },
+        manager::ManagerClient,
+    },
 };
 
 const PICTURE_SIZE: usize = 8 * 1024;
@@ -133,8 +149,56 @@ Inspired by "Web Service Frontend" demo from AIFM.
 This example is not intended to compare performance against AIFM, it is just their demo app is a good
 example software to integrate to.
 */
-pub fn run_web_service_demo() {
+pub fn run_web_service_demo(metrics: Registry, run_id: String, token: &str, storage_endpoints: Vec<String>, manager_endpoint: Option<String>, local_max_memory: Option<u64>) {
     info!("web service demo");
+
+    // far memory client init
+    let local_max_memory = local_max_memory.unwrap_or(20 * 1024 * 1024 * 1024);
+
+    let manager_client = manager_endpoint.map(|endpoint| {
+        let mut client = ManagerClient::new(&endpoint);
+        client.auth(token);
+        client
+    });
+
+    let backend: Box<dyn FarMemoryBackend> = if !storage_endpoints.is_empty() {
+        if storage_endpoints.len() == 1 {
+            info!("running in single backend node mode");
+            Box::new(NetworkNodeBackend::new(&storage_endpoints[0], token, run_id))
+        } else if storage_endpoints.len() == 5 {
+            info!("running in erasure coded mode");
+
+            let nodes: Vec<_> = storage_endpoints.iter()
+                .map(|v| Box::new(NetworkNodeBackend::new(&v, token, run_id.clone())) as Box<dyn FarMemoryBackend>)
+                .collect();
+
+            Box::new(ErasureCodingBackend::new(nodes))
+        } else {
+            let nodes: Vec<_> = storage_endpoints.iter()
+                .map(|v| Box::new(NetworkNodeBackend::new(&v, token, run_id.clone())) as Box<dyn FarMemoryBackend>)
+                .collect();
+
+            info!("running in replication mode with {} nodes", nodes.len());
+
+            Box::new(ReplicationBackend::new(nodes))
+        }
+    } else {
+        warn!("no storage endpoint provided, falling back to disk backend");
+        Box::new(LocalDiskBackend::new())
+    };
+
+    let backend = Box::new(InstrumentedBackend::new(metrics.clone(), backend));
+    let mut client = FarMemoryClient::new(backend, local_max_memory);
+    if let Some(manager) = manager_client {
+        let fallback = PreferRemoteSpansReplacementPolicy::new(Box::new(MostRecentlyUsedReplacementPolicy::new()));
+
+        client.use_replacement_policy(Box::new(RemoteReplayReplacementPolicy::new(manager.clone(), Box::new(fallback))));
+        client.use_manager(manager);
+    }
+    client.track_metrics(metrics.clone());
+    client.start_swap_out_thread();
+
+    // demo app
 
     let zipf_s = 0.8;
 
--- src/main.rs
+++ src/main.rs
@@ -166,7 +166,16 @@ pub fn main() {
         let run_id = generate_run_id();
         println!("run id: {:?}", run_id);
 
-        span!(Level::DEBUG, "web service demo").in_scope(|| run_web_service_demo());
+        let metrics = init_metrics(Some(run_id.clone()));
+
+        span!(Level::DEBUG, "web service demo").in_scope(|| run_web_service_demo(
+            metrics.clone(),
+            run_id.clone(),
+            &read_token(),
+            args.storage_endpoint.clone().map(|v| v.split(",").map(|v| v.to_owned()).collect::<Vec<String>>()).unwrap_or(Vec::new()),
+            args.manager_endpoint.clone(),
+            args.memory_limit_mb.map(|v| v * 1024 * 1024)
+        ));
     } else if args.benchmark {
         run_benchmark(&read_token(), args.storage_endpoint.clone());
     } else if args.block_device_demo {
