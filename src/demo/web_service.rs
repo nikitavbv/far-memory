551b957ab80bf1abb5c59e6f0f8318537a238666
Nikita Volobuev <nikitavbv@gmail.com>
2023-12-09 15:35:16 +0200

    feat(demo): handle requests in web service demo

--- src/demo/web_service.rs
+++ src/demo/web_service.rs
@@ -1,7 +1,8 @@
 use {
-    std::collections::HashMap,
+    std::{collections::HashMap, io::Write},
     tracing::info,
-    rand::{RngCore, Rng},
+    rand::{RngCore, Rng, rngs::OsRng},
+    aes_gcm::{aead::{KeyInit, Aead, AeadCore}, Aes256Gcm, Key},
 };
 
 const PICTURE_SIZE: usize = 8 * 1024 * 1024;
@@ -9,6 +10,8 @@ const PICTURE_SIZE: usize = 8 * 1024 * 1024;
 struct DemoWebService {
     users: HashMap<UserId, PictureId>,
     pictures: Vec<Picture>,
+
+    cipher: Aes256Gcm,
 }
 
 impl DemoWebService {
@@ -16,7 +19,43 @@ impl DemoWebService {
         Self {
             users,
             pictures,
+
+            cipher: Aes256Gcm::new(&Aes256Gcm::generate_key(OsRng)), // AIFM uses AES-CBC, but it doesn't matter for evaluation performance.
         }
+    }
+
+    pub fn handle_request(&self, request: WebServiceRequest) -> WebServiceResponse {
+        let picture_to_get: u64 = request.user_ids.iter()
+            .map(|id| self.users.get(id).unwrap().picture_id)
+            .sum();
+        let picture_to_get = picture_to_get % self.pictures.len() as u64; // TODO: zipf distribution
+
+        let picture = &self.pictures.get(picture_to_get as usize).unwrap().picture_data;
+        let encrypted_picture = self.encrypt_picture(picture);
+
+        // yes, encrpyted data cannot be compressed, but it still a good way to simulate CPU load. AIFM does the same for their evaluation.
+        let compressed_picture = self.compress_picture(&encrypted_picture);
+
+        WebServiceResponse::new(compressed_picture)
+    }
+
+    fn encrypt_picture(&self, picture: &[u8]) -> Vec<u8> {
+        let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
+        assert_eq!(12, nonce.len());
+
+        let encrypted = self.cipher.encrypt(&nonce, picture).unwrap();
+        {
+            let mut encrypted = encrypted;
+            let mut result = nonce.to_vec();
+            result.append(&mut encrypted);
+            result
+        }
+    }
+
+    fn compress_picture(&self, picture: &[u8]) -> Vec<u8> {
+        let mut output = Vec::new();
+        snap::write::FrameEncoder::new(&mut output).write_all(picture).unwrap();
+        output
     }
 }
 
@@ -71,14 +110,48 @@ impl WebServiceRequest {
     }
 }
 
-/** Inspired by "Web Service Frontend" demo from AIFM. */
+#[derive(Debug)]
+struct WebServiceResponse {
+    data: Vec<u8>,
+}
+
+impl WebServiceResponse {
+    pub fn new(data: Vec<u8>) -> Self {
+        Self {
+            data,
+        }
+    }
+}
+
+/**
+Inspired by "Web Service Frontend" demo from AIFM.
+
+This example is not intended to compare performance against AIFM, it is just their demo app is a good
+example software to integrate to.
+*/
 pub fn run_web_service_demo() {
     info!("web service demo");
 
     let pictures = generate_pictures(1000);
-    let users = generate_users(100, pictures.len());
+    info!("finished generating pictures");
+
+    let total_users = 100;
+    let users = generate_users(total_users, pictures.len());
+    info!("finished generating users");
 
     let web_service = DemoWebService::new(users, pictures);
+
+    let request = random_request(total_users);
+    let response = web_service.handle_request(request);
+
+}
+
+fn random_request(total_users: usize) -> WebServiceRequest {
+    WebServiceRequest::new((0..32).map(|_| generate_user_id(total_users)).collect())
+}
+
+fn generate_user_id(total_users: usize) -> UserId {
+    UserId::new(rand::thread_rng().gen_range(0..total_users as u64))
 }
 
 fn generate_users(total_users: usize, total_pictures: usize) -> HashMap<UserId, PictureId> {
