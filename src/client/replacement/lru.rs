2661a5976d6a6a6ba0008211172d1b557cd00728
Nikita Volobuev <nikitavbv@gmail.com>
2023-12-23 18:14:25 +0200

    refactor(replacement): simplify lru

--- src/client/replacement/lru.rs
+++ src/client/replacement/lru.rs
@@ -8,7 +8,6 @@ use {
 pub struct LeastRecentlyUsedReplacementPolicy {
     counter: AtomicU64,
     history: RwLock<HashMap<SpanId, u64>>,
-    remote_spans: RwLock<HashSet<SpanId>>,
 }
 
 impl LeastRecentlyUsedReplacementPolicy {
@@ -16,7 +15,6 @@ impl LeastRecentlyUsedReplacementPolicy {
         Self {
             counter: AtomicU64::new(0),
             history: RwLock::new(HashMap::new()),
-            remote_spans: RwLock::new(HashSet::new()),
         }
     }
 }
@@ -24,24 +22,24 @@ impl LeastRecentlyUsedReplacementPolicy {
 impl ReplacementPolicy for LeastRecentlyUsedReplacementPolicy {
     fn pick_for_eviction<'a>(&self, spans: &'a[SpanId]) -> &'a SpanId {
         let history = span!(Level::DEBUG, "acquiring history lock").in_scope(|| self.history.read().unwrap());
-        let remote_spans = span!(Level::DEBUG, "acquiring remote spans lock").in_scope(|| self.remote_spans.read().unwrap());
         span!(Level::DEBUG, "filtering spans").in_scope(|| spans.iter()
-            .filter(|v| !remote_spans.contains(v))
-            .map(|v| (v, history.get(v).unwrap_or(&0)))
+            .map(|v| (v, history.get(v)))
+            .filter(|v| v.1.is_some())
+            .map(|v| (v.0, v.1.unwrap()))
             .reduce(|a, b| if a.1 < b.1 { a } else { b }).map(|a| a.0).unwrap())
     }
 
     fn on_span_access(&self, span_id: &SpanId) {
-        self.history.write().unwrap().insert(span_id.clone(), self.counter.fetch_add(1, Ordering::Relaxed));;
+        self.history.write().unwrap().insert(span_id.clone(), self.counter.fetch_add(1, Ordering::Relaxed));
     }
 
     fn on_span_swap_out(&self, span_id: &SpanId, partial: bool) {
         if !partial {
-            self.remote_spans.write().unwrap().insert(span_id.clone());
+            self.history.write().unwrap().remove(&span_id);
         }
     }
 
-    fn on_span_swap_in(&self, span_id: &SpanId) {
-        self.remote_spans.write().unwrap().remove(span_id);
+    fn on_span_swap_in(&self, _span_id: &SpanId) {
+        // do nothing
     }
 }
