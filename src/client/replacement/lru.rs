5c56a34178caecab8675855a1e0f7f37b59ae635
Nikita Volobuev <nikitavbv@gmail.com>
2023-12-24 19:00:16 +0200

    feat(replacement): optimized lru

--- src/client/replacement/lru.rs
+++ src/client/replacement/lru.rs
@@ -1,49 +1,44 @@
 use {
-    std::{sync::{atomic::{AtomicU64, Ordering}, RwLock}, collections::{HashMap, HashSet}},
+    std::sync::RwLock,
     tracing::{span, Level},
+    lru::LruCache,
     crate::client::{SpanId, ReplacementPolicy},
 };
 
 // 108.3 per token (for 25700)
 pub struct LeastRecentlyUsedReplacementPolicy {
-    counter: AtomicU64,
-    history: RwLock<HashMap<SpanId, u64>>,
+    cache: RwLock<LruCache<SpanId, ()>>,
 }
 
 impl LeastRecentlyUsedReplacementPolicy {
     pub fn new() -> Self {
         Self {
-            counter: AtomicU64::new(0),
-            history: RwLock::new(HashMap::new()),
+            cache: RwLock::new(LruCache::unbounded()),
         }
     }
 }
 
 impl ReplacementPolicy for LeastRecentlyUsedReplacementPolicy {
-    fn pick_for_eviction(&self, spans: &[SpanId]) -> SpanId {
-        let history = span!(Level::DEBUG, "acquiring history lock").in_scope(|| self.history.read().unwrap());
-        span!(Level::DEBUG, "filtering spans").in_scope(|| spans.iter()
-            .map(|v| (v, history.get(v)))
-            .filter(|v| v.1.is_some())
-            .map(|v| (v.0, v.1.unwrap()))
-            .reduce(|a, b| if a.1 < b.1 { a } else { b }).map(|a| a.0).unwrap().clone())
+    fn pick_for_eviction(&self, _spans: &[SpanId]) -> SpanId {
+        let cache = span!(Level::DEBUG, "acquiring cache lock").in_scope(|| self.cache.read().unwrap());
+        span!(Level::DEBUG, "filtering spans").in_scope(|| cache.peek_lru().unwrap().0.clone())
     }
 
     fn on_new_span(&self, span_id: &SpanId) {
-        self.on_span_access(span_id);
+        self.cache.write().unwrap().put(span_id.clone(), ());
     }
 
     fn on_span_access(&self, span_id: &SpanId) {
-        self.history.write().unwrap().insert(span_id.clone(), self.counter.fetch_add(1, Ordering::Relaxed));
+        self.cache.write().unwrap().promote(span_id);
     }
 
     fn on_span_swap_out(&self, span_id: &SpanId, partial: bool) {
         if !partial {
-            self.history.write().unwrap().remove(&span_id);
+            self.cache.write().unwrap().pop_entry(span_id);
         }
     }
 
-    fn on_span_swap_in(&self, _span_id: &SpanId) {
-        // do nothing
+    fn on_span_swap_in(&self, span_id: &SpanId) {
+        self.cache.write().unwrap().put(span_id.clone(), ());
     }
 }
