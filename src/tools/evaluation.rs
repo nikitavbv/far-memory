12097aabf156ee6adb9b26177240cbb26b41c1eb
Nikita Volobuev <nikitavbv@gmail.com>
2024-01-14 17:27:34 +0200

    feat(demo): reduce dataframe demo to 10k entries

--- src/tools/evaluation.rs
+++ src/tools/evaluation.rs
@@ -71,7 +71,7 @@ impl DemoApplicationType {
         match self {
             Self::LlmInference => "llm_inference",
             Self::WebService => "web_service_v2",
-            Self::Dataframe => "dataframe_v2_1m",
+            Self::Dataframe => "dataframe_v2_10k",
         }.to_owned()
     }
 }
