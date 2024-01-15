fe8297abee8c95cc7ebd89821ebee9afcb04209b
Nikita Volobuev <nikitavbv@gmail.com>
2024-01-15 23:18:29 +0200

    feat(infrastructure): add app server

--- infrastructure/terraform/demo/main.tf
+++ infrastructure/terraform/demo/main.tf
@@ -7,5 +7,18 @@ terraform {
 }
 
 provider equinix {
-    auth_token = file("./secrets/equinix_auth_token")
+    auth_token = file("./.secrets/equinix_auth_token")
+}
+
+data local_file project_id {
+    filename = "./.secrets/equinix_project_id"
+}
+
+resource equinix_metal_device far_memory_app {
+    hostname = "far-memory-app"
+    plan = "m3.small.x86" # has 25gbps NIC
+    metro = "fr"
+    operating_system = "ubuntu_22_04"
+    billing_cycle = "hourly"
+    project_id = data.local_file.project_id.content
 }
