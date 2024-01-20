9a2a111c29d889a1376290a684e5e97aa2ce93e4
Nikita Volobuev <nikitavbv@gmail.com>
2024-01-20 18:07:24 +0200

    feat(demo): add network performance benchmark

--- src/demo/benchmark.rs
+++ src/demo/benchmark.rs
@@ -1,9 +1,10 @@
 use {
-    std::time::Instant,
-    tokio::{net::TcpListener, io::{AsyncWriteExt, AsyncReadExt}},
-    rand::Rng,
+    std::{time::Instant, net::{TcpListener, TcpStream}, io::{Read, Write}},
+    rand::RngCore,
     tracing::{info, span, Level},
 };
+
+const DATA_TO_TRANSFER: usize = 200 * 1024 * 1024; // 200MB is typical payload
 
 pub fn run_benchmark(token: &str, endpoint: Option<String>) {
     info!("running benchmark");
@@ -18,46 +19,34 @@ pub fn run_benchmark(token: &str, endpoint: Option<String>) {
 fn run_server() {
     info!("running benchmark server");
 
-    let rt = tokio::runtime::Runtime::new().unwrap();
-    rt.block_on(async {
-        let listener = TcpListener::bind("0.0.0.0:14000").await.unwrap();
+    let listener = TcpListener::bind("0.0.0.0:14000").unwrap();
+    let mut buffer = vec![0u8; DATA_TO_TRANSFER];
 
-        loop {
-            let (mut socket, _) = listener.accept().await.unwrap();
-            socket.set_nodelay(true).unwrap();
+    for stream in listener.incoming() {
+        let mut stream = stream.unwrap();
 
-            let mut rng = rand::thread_rng();
-            let data: Vec<u8> = (0..180355072).map(|_| rng.gen()).collect();
+        let started_at = Instant::now();
+        stream.read_exact(&mut buffer).unwrap();
+        info!("server finished reading in {:?}", Instant::now() - started_at);
 
-            let (mut reader, mut writer) = socket.split();
-
-            let started_at = Instant::now();
-            let data_len = data.len();
-            let write_response_task = async move {
-                writer.write(&(data.len() as u64).to_be_bytes()).await.unwrap();
-                writer.write(&data).await.unwrap();
-            };
-
-            let read_request_task = async move {
-                let req_len = {
-                    let mut req_len: [u8; 8] = [0u8; 8];
-                    reader.read_exact(&mut req_len).await.unwrap();
-                    u64::from_be_bytes(req_len)
-                };
-
-                {
-                    let mut req = vec![0u8; req_len as usize];
-                    reader.read_exact(&mut req).await.unwrap();
-                    req
-                }
-            };
-
-            let (_, res) = tokio::join!(write_response_task, read_request_task);
-            info!("processed request in: {:?}, received {}, sent {}", Instant::now() - started_at, res.len(), data_len);
-        }
-    });
+        let started_at = Instant::now();
+        stream.write_all(&buffer).unwrap();
+        info!("server finished writing in {:?}", Instant::now() - started_at);
+    }
 }
 
 fn run_client(endpoint: String) {
-    unimplemented!()
+    let mut data = vec![0u8; DATA_TO_TRANSFER];
+    rand::thread_rng().fill_bytes(&mut data);
+
+    let mut stream = TcpStream::connect(&endpoint).unwrap();
+    stream.set_nodelay(true).unwrap();
+
+    let started_at = Instant::now();
+    stream.write_all(&data).unwrap();
+    info!("client finished reading in {:?}", Instant::now() - started_at);
+
+    let started_at = Instant::now();
+    stream.read_exact(&mut data).unwrap();
+    info!("client finished reading in {:?}", Instant::now() - started_at);
 }
